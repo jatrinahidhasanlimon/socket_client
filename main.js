51e949f0a9074eeb8b2d7aca9744efe59b9a0201
NahidHasanLimon <nh.limon@yahoo.com>
2022-09-17 03:47:46 +0600

    previous messsage

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 // console.clear();
 const chatMainContainer= document.getElementById("chatMainContainer");
 const loginMainContainer = document.getElementById("loginMainContainer");
+
+const validationMessageElem = document.getElementById("validationMessage");
+
+
 const userNameElem = document.getElementById("username");
 const bookingIDElem = document.getElementById("booking_id");
-const validationMessageElem = document.getElementById("validationMessage");
+const userIDElem = document.getElementById("userid");
+const avatarUrlElem = document.getElementById("avata_url");
+const loggedUserNamelElem = document.getElementById("logged_user_name");
+const loggedBookingIDlElem = document.getElementById("logged_booking_id");
+
+
 function checkAuth(){
   var url = 'http://localhost:7000/rental/start-chat';
   
@@ -35,52 +44,120 @@ function showLoginToggle(){
     loginMainContainer.classList.add('d-none')
     }
 }
+
 function startChat(){
-  checkAuth()
-
-
+  // checkAuth()
   validationMessageElem.innerHTML = "";
   console.log('username is: ', userNameElem.value)
   console.log('bookingID is: ', bookingIDElem.value)
   if(userNameElem.value && bookingIDElem.value){
+  // if(userNameElem){
     validationMessageElem.innerHTML = "";
     loginMainContainer.classList.add("d-none");
     chatMainContainer.classList.remove("d-none");
+    loggedUserNamelElem.textContent += userNameElem.value;
+    loggedBookingIDlElem.textContent += bookingIDElem.value;
+    //
+    const userDetails = {
+      userID: userIDElem.value,
+      bookingID: bookingIDElem.value,
+      userName: userNameElem.value,
+      avatarUrl: avatarUrlElem.value,
+    }
+    
+    const socket = io("http://localhost:3000",{
+      auth: {
+        token: "abcdefghij",
+        data: userDetails
+      }
+    });
+    
+    socket.on("connect", () => {
+      console.log(socket.connected); // true
+    });
+    socket.emit('adduser', {
+      user: {
+        userID: userIDElem.value,
+        userName: userNameElem.value,
+        avatarUrl: avatarUrlElem.value,
+      },
+      bookingID: bookingIDElem.value
+    });
+
+    socket.on("newMessage", (response) => {
+      console.log('response is', response); 
+    });
+    
+    
+    socket.on("aNewChatMessageReceived", (data) => {
+      console.log('Called aNewChatMessageReceived')
+      console.log('chat text is', data); 
+      appendChatText(data)
+    });
+    socket.on("connect_error", (err) => {
+      console.log(err instanceof Error); // true
+      console.log(err.message); // not authorized
+      console.log(err.data); // { content: "Please retry later" }
+    });
+
+    sendMessage.addEventListener("click", (e)=> {
+      if(message.value){
+        let text = document.createElement("div");
+        let profilePicContainer = document.createElement("div");
+        let pic = document.createElement("img");
+        let textContent = document.createElement("div");
+        let timeStamp = document.createElement("span");
+        let name = document.createElement("h5");
+        
+        // okay 
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes() + "";
+        let time = `${hours}:${minutes.padStart(2, "0")}`;
+    
+        name.innerText = "Laurie";
+        timeStamp.classList.add("timestamp");
+        timeStamp.innerText = time;
+        textContent.classList.add("text-content");
+        textContent.appendChild(name);
+        textContent.append(message.value);
+        textContent.appendChild(timeStamp);
+        // pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+        pic.setAttribute("src", avatarUrlElem.value);
+        text.classList.add("text");
+        text.classList.add("sent");
+        profilePicContainer.classList.add("profile-pic");
+        profilePicContainer.appendChild(pic);
+        text.appendChild(profilePicContainer);
+        text.appendChild(textContent);
+        chatContainer.appendChild(text);
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+        // for socket 
+        socket.emit('createMessage', message.value);
+        // for socket 
+    
+        message.value = "";
+        
+      }
+      
+    
+      
+    });
+
   }else{
     validationMessageElem.innerHTML += 
-    "<h6 class='text-danger'>Please Enter Username and Booking ID</h6>";
+    "<h6 class='text-danger'>Please Enter Username, Userid, avatar url and Booking ID</h6>";
   }
   
 
 }
 
-const userDetails = {
-  name: 'user 1'
-}
-const socket = io("http://localhost:3000",{
-  auth: {
-    token: "abcdefghij"
-  }
-});
-
-socket.on("connect", () => {
-  console.log(socket.connected); // true
-  socket.emit('adduser', 'user1');
-});
-
-socket.on("newMessage", (response) => {
-  console.log('response is', response); 
-});
 
 
-socket.on("aNewChatMessageReceived", (newMessage) => {
-  console.log('Called aNewChatMessageReceived')
-  console.log('chat text is', newMessage); 
-  appendChatText(newMessage)
-});
 
 
-function appendChatText (){
+
+function appendChatText (data){
   let text = document.createElement("div");
   let profilePicContainer = document.createElement("div");
   let pic = document.createElement("img");
@@ -101,7 +178,8 @@ function appendChatText (){
   textContent.appendChild(name);
   textContent.append(message.value);
   textContent.appendChild(timeStamp);
-  pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+  // pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+  pic.setAttribute("src", avatarUrlElem.value);
   text.classList.add("text");
   profilePicContainer.classList.add("profile-pic");
   profilePicContainer.appendChild(pic);
@@ -135,61 +213,11 @@ text.forEach(el=>{
 });
 */
 
-sendMessage.addEventListener("click", (e)=> {
-  
-  if(message.value){
-    
-    let text = document.createElement("div");
-    let profilePicContainer = document.createElement("div");
-    let pic = document.createElement("img");
-    let textContent = document.createElement("div");
-    let timeStamp = document.createElement("span");
-    let name = document.createElement("h5");
-    
-    // okay 
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes() + "";
-    let time = `${hours}:${minutes.padStart(2, "0")}`;
-
-    name.innerText = "Laurie";
-    timeStamp.classList.add("timestamp");
-    timeStamp.innerText = time;
-    textContent.classList.add("text-content");
-    textContent.appendChild(name);
-    textContent.append(message.value);
-    textContent.appendChild(timeStamp);
-    pic.setAttribute("src", "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-    text.classList.add("text");
-    text.classList.add("sent");
-    profilePicContainer.classList.add("profile-pic");
-    profilePicContainer.appendChild(pic);
-    text.appendChild(profilePicContainer);
-    text.appendChild(textContent);
-    chatContainer.appendChild(text);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-    // for socket 
-    sendMessageViaSocket(message.value)
-    // for socket 
-
-    message.value = "";
-    
-  }
-  function sendMessageViaSocket(message){
-    console.log('From sendMessageViaSocket')
-    socket.emit('createMessage', message);
-  }
-
-  
-});
 
 
 
 
-socket.on("connect_error", (err) => {
-  console.log(err instanceof Error); // true
-  console.log(err.message); // not authorized
-  console.log(err.data); // { content: "Please retry later" }
-});
+
+
 
 
